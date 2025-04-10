3c1e866869b0d9df2ddb12a9a361c51e1ce6aa55
OwenxOwen <oreynold@ashland.edu>
2025-04-10 12:03:37 -0400

    Started Header

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -4,7 +4,25 @@ import './Header.css';
 export default function Header() {
     return(
         <>
-        
+            <div className="headerContainer">
+                <div className="headerContent">
+                    <div className="leftHeaderSection">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="middleHeaderSection">
+                        <h2>Vehicles</h2>
+                        <h2>Energy</h2>
+                        <h2>Charging</h2>
+                        <h2>Discover</h2>
+                        <h2>Shop</h2>
+                    </div>
+                    <div className="rightHeaderSection">
+                        <i class="fa-solid fa-question"></i>
+                        <i class="fa-solid fa-globe"></i>
+                        <i class="fa-solid fa-person"></i>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
 
+import  Header  from '../../components/Header/Header.jsx';
+
 export default function Home() {    
     return(
         <>
-            
+            <Header />
         </>
     );
 }
